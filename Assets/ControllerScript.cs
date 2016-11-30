bbb4a660be2eae2db829cd779644a5b945fed5cc
konlil <konlil@163.com>
2016-11-30 09:30:12 +0800

    first commit

---
+++ Assets/ControllerScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllerScript : MonoBehaviour {
+
+	float forwardSpeed = 30.0f;
+	float turnSpeed = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float turnAmount = Input.GetAxis ("Horizontal") * turnSpeed;
+		float forwarAmount = Input.GetAxis ("Vertical") * forwardSpeed;
+
+		transform.Rotate (0, turnAmount, 0);
+
+		GetComponent<Rigidbody>().AddRelativeForce (forwarAmount, 0, 0);
+	}
+}
